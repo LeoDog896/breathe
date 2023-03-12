5468d4406cefe48a4f4bf50c74123bf5c777f841
Tristan F <LeoDog896@hotmail.com>
2023-03-12 19:57:43 -0400

    chore: re-init w/ sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import "../app.css" 
+    import { Canvas, Layer, t, type Render } from "svelte-canvas";
+
+    let render: Render;
+    $: render = ({ context, width, height }) => {
+        context.beginPath();
+        context.arc(width / 2, height / 2, Math.min(width, height) / 2, 0, 2 * Math.PI);
+        context.fillStyle = "rgba(0, 0, 255, 0.1)";
+        context.fill();
+    };
+</script>
+
+<Canvas width={300} height={300} class="canvas">
+    <Layer {render} />
+</Canvas>
+
+<style>
+    :global(.canvas) {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+    }
+</style>
