357430aa7c031eca0f3b6856d2042403989c1731
Tristan F <LeoDog896@hotmail.com>
2023-03-12 20:46:03 -0400

    feat: tweened motion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
     import "../app.css" 
+    import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
+	const radius = tweened(0, {
+		duration: 4000,
+		easing: cubicOut
+	});
 
     let dimension = 300;
+
+    radius.set(dimension)
 </script>
 
 <div class="background-circle" style="width: {dimension}px; height: {dimension}px;"></div>
-<div class="foreground-circle"></div>
+<div class="foreground-circle" style="width: {$radius}px; height: {$radius}px;"></div>
 
 <style>
     div {
@@ -14,6 +23,10 @@
         left: 50%;
         transform: translate(-50%, -50%);
         border-radius: 50%;
+    }
+
+    .foreground-circle {
+        background-color: rgba(0, 0, 255, 0.5);
     }
 
     .background-circle {
